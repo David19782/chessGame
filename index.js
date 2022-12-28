69c68f5add8ce020c48b6012bbc791fb979b9a96
David19782 <56271000+David19782@users.noreply.github.com>
2022-12-29 01:23:19 +0200

    update board
    make another board with figure. Next step add movement to figure

--- index.js
+++ index.js
@@ -1,152 +1,290 @@
-const urls = {
-    "white_king": "./images/white_king-removebg-preview.png",
-    "black_king": "./images/black_king_2-removebg-preview.png",
-    "black_queen": "./images/black_queen-removebg-preview.png",
-    "white_queen": "./images/white_queen-removebg-preview.png",
-    "white_bishup": "./images/white_bishup-removebg-preview.png",
-    "black_bishup": "./images/black_bishup-removebg-preview.png",
-    "white_horse": "./images/white_horse-removebg-preview.png",
-    "black_horse": "./images/black_horse-removebg-preview.png",
-    "white_top": "./images/white_top-removebg-preview.png",
-    "black_top": "./images/black_top-removebg-preview.png",
-    "white_paw": "./images/white_paw-removebg-preview.png",
-    "black_paw": "./images/black_paw-removebg-preview.png"
-}
-
-function makeImg(type){
-    let curr = documet.createElement("img");
-    curr.setAttribute("class", type);
-    curr.setAttribute("src", urls[type]);
-    return curr;
-}
-
-class Paw {
-    constructor(position, type, url){
-        this.position = position;
-        this.type = type;
-        this.url = url;
-    }
-    move() {
-        if(this.type == "white"){
-            return [[position[0] - 1], [position[1] - 1]];
-        }else{
-            return [[position[0] + 1], [position[1] - 1]];   
-        }
-    }
-}
-
-class White_Player{
-    constructor (){
-        this.paws[8] = [
-            new Paw([1, 0], "white", urls["white_paw"]), 
-            new Paw([1, 1], "white", urls["white_paw"]),
-            new Paw([1, 2], "white", urls["white_paw"]),
-            new Paw([1, 3], "white", urls["white_paw"]),
-            new Paw([1, 4], "white", urls["white_paw"]),
-            new Paw([1, 5], "white", urls["white_paw"]),
-            new Paw([1, 6], "white", urls["white_paw"]),
-            new Paw([1, 7], "white", urls["white_paw"]),
-            new Paw([1, 8], "white", urls["white_paw"])
-        ];
-        
-    }
-
-    display(){
-        let m = [];
-        for(let i = 0; i < this.paws.length; i++){
-            m.push([[...this.paws[i].position], makeImg("white_paw")]);
-        } 
-        return m;
-    }
-}
-
-/*class Black_Player{
-    constructor (){
-        this.paws[8] = [[1,0], [1,1], [1,2], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7]];
-        this.horses[2] = [[0, 1], [0, 6]];
-        this.bishups[2] = [[0, 2], [0, 5]];
-        this.top[2] = [[0, 0], [0, 7]];
-        this.quenn = [[0,3]];
-        this.king = [[0, 4]];
-    }
-}
-
-*/
-
-
-
 window.addEventListener("load", () => {
     const game = document.getElementsByClassName("game")[0];
-    let table = [];
-    function test() {
-        let currImg = document.createElement("img");
-        currImg.setAttribute("src", "./images/black_king_2-removebg-preview.png");
-        currImg.setAttribute("class", "black_king");
-        console.log(table[0][0]);
-        let currImg2 = document.createElement("img");
-        currImg2.setAttribute("src", "./images/white_king-removebg-preview.png");
-        let currImg3 = document.createElement("img");
-        currImg3.setAttribute("src", "./images/white_queen-removebg-preview.png");
-        currImg3.setAttribute("class", "white_queen");
-        let currImg4 = document.createElement("img");
-        currImg4.setAttribute("src", "./images/black_queen-removebg-preview.png");
-        currImg4.setAttribute("class", "black_queen");
-        let currImg5 = document.createElement("img");
-        currImg5.setAttribute("src", "./images/white_bishup-removebg-preview.png");
-        currImg5.setAttribute("class", "white_bishup");
-        let currImg6 = document.createElement("img");
-        currImg6.setAttribute("src", "./images/black_bishup-removebg-preview.png");
-        currImg6.setAttribute("class", "black_bishup");
-        let currImg7 = document.createElement("img");
-        currImg7.setAttribute("src", "./images/white_horse-removebg-preview.png");
-        currImg7.setAttribute("class", "white_horse");
-        let currImg8 = document.createElement("img");
-        currImg8.setAttribute("src", "./images/black_horse-removebg-preview.png");
-        currImg8.setAttribute("class", "black_horse");
-        let currImg9 = document.createElement("img");
-        currImg9.setAttribute("src", "./images/white_top-removebg-preview.png");
-        currImg9.setAttribute("class", "white_top");
-        let currImg10 = document.createElement("img");
-        currImg10.setAttribute("src", "./images/black_top-removebg-preview.png");
-        currImg10.setAttribute("class", "black_top");
-        let currImg11 = document.createElement("img");
-        currImg11.setAttribute("src", "./images/white_paw-removebg-preview.png");
-        currImg11.setAttribute("class", "white_paw");
-        let currImg12 = document.createElement("img");
-        currImg12.setAttribute("src", "./images/black_paw-removebg-preview.png");
-        currImg12.setAttribute("class", "black_paw");
+    // make figure
+    const urls = {
+        "white_king": "./images/white_king-removebg-preview.png",
+        "black_king": "./images/black_king_2-removebg-preview.png",
+        "black_queen": "./images/black_queen-removebg-preview.png",
+        "white_queen": "./images/white_queen-removebg-preview.png",
+        "white_bishup": "./images/white_bishup-removebg-preview.png",
+        "black_bishup": "./images/black_bishup-removebg-preview.png",
+        "white_horse": "./images/white_horse-removebg-preview.png",
+        "black_horse": "./images/black_horse-removebg-preview.png",
+        "white_top": "./images/white_top-removebg-preview.png",
+        "black_top": "./images/black_top-removebg-preview.png",
+        "white_paw": "./images/white_paw-removebg-preview.png",
+        "black_paw": "./images/black_paw-removebg-preview.png"
+    }
     
-        table[4][5].appendChild(currImg);
-        table[0][2].appendChild(currImg2);
-        table[7][5].appendChild(currImg3);
-        table[6][2].appendChild(currImg4);
-        table[3][4].appendChild(currImg5);
-        table[7][3].appendChild(currImg6);
-        table[5][5].appendChild(currImg7);
-        table[6][3].appendChild(currImg8);
-        table[1][1].appendChild(currImg9);
-        table[5][4].appendChild(currImg10);
-        table[2][3].appendChild(currImg11);
-        table[2][2].appendChild(currImg12);
+    function makeImg(type){
+        let curr = document.createElement("img");
+        curr.setAttribute("class", type);
+        curr.setAttribute("src", urls[type]);
+        return curr;
+    }
+    
+    class Paw {
+        constructor(position, type, url){
+            this.position = position;
+            this.type = type;
+            this.url = url;
+        }
+        
+        move(table, position, type) {
+            let avMoves = [];
+            let [i, j] = position;
+            if(type == "white"){
+                if(table[i - 1][j] == undefined){
+                    avMoves.push([i - 1, j]);
+                    if(i == 6 && table[i - 2][j] == undefined){
+                        avMoves.push([i - 2, j]);
+                    }
+                }
+                
+            }else{
+
+            }
+            return avMoves;
+            
+        }
     }
 
+    class King{
+        constructor(position, type, url) {
+            this.position = position;
+            this.type = type;
+            this.url = url;
+        }
+    }
+
+    class Queen{
+        constructor(position, type, url) {
+            this.position = position;
+            this.type = type;
+            this.url = url;
+        }
+    }
+    class Horse{
+        constructor(position, type, url) {
+            this.position = position;
+            this.type = type;
+            this.url = url;
+        }
+    }
+    class Bishup{
+        constructor(position, type, url) {
+            this.position = position;
+            this.type = type;
+            this.url = url;
+        }
+    }
+    class Top{
+        constructor(position, type, url) {
+            this.position = position;
+            this.type = type;
+            this.url = url;
+        }
+    }
+    
+    // make players
+
+    class Black_Player{
+        constructor() {
+            this.paws = [
+                new Paw([1, 0], "black", urls["black_paw"]),
+                new Paw([1, 1], "black", urls["black_paw"]), 
+                new Paw([1, 2], "black", urls["black_paw"]), 
+                new Paw([1, 3], "black", urls["black_paw"]), 
+                new Paw([1, 4], "black", urls["black_paw"]), 
+                new Paw([1, 5], "black", urls["black_paw"]), 
+                new Paw([1, 6], "black", urls["black_paw"]), 
+                new Paw([1, 7], "black", urls["black_paw"]), 
+            ];
+            this.king = new King([0, 4], "black", urls["black_king"]);
+            this.queen = new Queen([0, 3], "black", urls["black_king"]);
+            this.bishups = [
+                new Bishup([0, 2], "black", urls["black_bishup"]),
+                new Bishup([0, 5], "black", urls["black_bishup"])
+            ]
+            this.horses = [
+                new Horse([0, 1], "black", urls["black_horse"]),
+                new Horse([0, 6], "black", urls["black_horse"])
+            ]
+            this.tops = [
+                new Top([0, 0], "black", urls["black_top"]),
+                new Top([0, 7], "black", urls["black_top"])
+            ]
+        }
+
+        display() {
+            let m = [];
+            for(let i = 0; i < this.paws.length; i++){
+                m.push([[...this.paws[i].position], makeImg("black_paw")]);
+            }
+            if(this.king)m.push([[...this.king.position], makeImg("black_king")]);
+            if(this.queen)m.push([[...this.queen.position], makeImg("black_queen")]);
+            for(let i = 0; i < this.bishups.length; i++){
+                m.push([[...this.bishups[i].position], makeImg("black_bishup")]);
+            }
+            for(let i = 0; i < this.horses.length; i++){
+                m.push([[...this.horses[i].position], makeImg("black_horse")]);
+            }
+            for(let i = 0; i < this.tops.length; i++){
+                m.push([[...this.tops[i].position], makeImg("black_top")]);
+            }
+            return m;
+        }
+
+    }
+
+    class White_Player{
+        constructor (){
+            this.paws = [
+                new Paw([6, 0], "white", urls["white_paw"]), 
+                new Paw([6, 1], "white", urls["white_paw"]),
+                new Paw([6, 2], "white", urls["white_paw"]),
+                new Paw([6, 3], "white", urls["white_paw"]),
+                new Paw([6, 4], "white", urls["white_paw"]),
+                new Paw([6, 5], "white", urls["white_paw"]),
+                new Paw([6, 6], "white", urls["white_paw"]),
+                new Paw([6, 7], "white", urls["white_paw"])
+            ];
+            this.king = new King([7, 4], "white", urls["white_king"]);
+            this.queen = new Queen([7, 3], "white", urls["white_queen"]);
+            this.bishups = [
+                new Bishup([7, 5], "white", urls["white_bishup"]),
+                new Bishup([7, 2], "white", urls["white_bishup"])
+            ]
+            this.horses = [
+                new Horse([7, 1], "white", urls["white_horse"]),
+                new Horse([7, 6], "white", urls["white_horse"])
+            ]
+            this.tops = [
+                new Top([7, 0], "white", urls["white_top"]),
+                new Top([7, 7], "white", urls["white_top"])
+            ]
+        }
+    
+        display(){
+            let m = [];
+            for(let i = 0; i < this.paws.length; i++){
+                m.push([[...this.paws[i].position], makeImg("white_paw")]);
+            }
+            if(this.king)m.push([[...this.king.position], makeImg("white_king")]) 
+            if(this.queen)m.push([[...this.queen.position], makeImg("white_queen")]);
+            for(let i = 0; i < this.bishups.length; i++){
+                m.push([[...this.bishups[i].position], makeImg("white_bishup")])
+            }
+            for(let i = 0; i < this.horses.length; i++){
+                m.push([[...this.horses[i].position], makeImg("white_horse")]);
+            }
+            for(let i = 0; i < this.tops.length; i++){
+                m.push([[...this.tops[i].position], makeImg("white_top")]);
+            }
+            return m;
+        }
+    }
+
+    // and gen start table
+
+    let table = [];
+    let pole = [];
+    for(let i = 0; i < 8; i++){
+        pole[i] = [];
+        if(i == 1){
+            for(let j = 0; j < 8; j++)pole[i][j] = new Paw([i, j], "black", "black_paw");
+            for(let j = 0; j < 8; j++)pole[6][j] = new Paw([6, j], "white", "white_paw");
+        }
+        if(i == 0){
+            for(let j = 0; j < 8; j++){
+                if(j == 0 || j == 7){
+                    pole[i][j] = new Top([i, j], "black", "black_top");
+                }
+                if(j == 1 || j == 6){
+                    pole[i][j] = new Bishup([i, j], "black", "black_bishup");
+                }
+                if(j == 2 || j == 5){
+                    pole[i][j] = new Horse([i, j], "black", "black_horse");
+                }
+                if(j == 3){
+                    pole[i][j] = new King([i, j], "black", "black_king");
+                }            
+                if(j == 4){
+                    pole[i][j] = new Queen([i, j], "black", "black_king");
+                }
+            }
+        }
+        if(i == 7){
+            for(let j = 0; j < 8; j++){
+                if(j == 0 || j == 7){
+                    pole[i][j] = new Top([i, j], "white", "white_top");
+                }
+                if(j == 1 || j == 6){
+                    pole[i][j] = new Bishup([i, j], "white", "white_bishup");
+                }
+                if(j == 2 || j == 5){
+                    pole[i][j] = new Horse([i, j], "white", "white_horse");
+                }
+                if(j == 3){
+                    pole[i][j] = new King([i, j], "white", "white_king");
+                }            
+                if(j == 4){
+                    pole[i][j] = new Queen([i, j], "white", "white_king");
+                }
+            }
+        }
+    }
+    // make test
+
+    
     function genTable(){
         for(let i = 0; i < 8; i++){
             table[i] = [];
             for(let j = 0; j < 8; j++){
                 table[i][j] = document.createElement("div");
                 table[i][j].setAttribute("class", "block");
-               // table[i][j].textContent = `${i}_${j}`;
+            // table[i][j].textContent = `${i}_${j}`;
+                table[i][j].setAttribute("id", `${i}_${j}`);
                 if(i % 2 == j % 2){
                     table[i][j].setAttribute("class", "white");
                 }else{
                     table[i][j].setAttribute("class", "black");
                 }
-                game.appendChild(table[i][j]);
+            game.appendChild(table[i][j]);
             }
         }
     }
     genTable();
+    let player1 = new White_Player();
+    let player2 = new Black_Player();
+    player1.display().forEach(el => {
+        //console.log(el);
+        table[el[0][0]][el[0][1]].appendChild(el[1]);
+    })
+    player2.display().forEach(el => {
+        //console.log(el);
+        table[el[0][0]][el[0][1]].appendChild(el[1]);
+    })
+    //test();
+    
+    // make events
+    let selected = false;
+    document.getElementsByClassName("game")[0].addEventListener("click", (e) => {
+        //console.log(e.target);
+        let curr;
+        if(e.target.tagName == "IMG"){
+           //   console.log(e.target.parentElement)
+            curr = e.target.parentElement;
+        }else{
+            curr = e.target;
+        }
+        console.log(curr.children)
+        if(!selected && curr.children.length == 0)return;
+        console.log(curr.children[0]);
+        selected = true;
+
+        
+    })
     
 
 })
